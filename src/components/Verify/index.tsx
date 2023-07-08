0bed94d01fc74f1c1561f65699b37a921b958e13
Arvind Kumar <arvind@earnnest.me>
2023-07-08 08:20:22 +0530

    dark theme css

--- src/components/Verify/index.tsx
+++ src/components/Verify/index.tsx
@@ -69,14 +69,11 @@ export default function Verify() {
           OTP sent to {email}
         </p>
         <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="otp"
-          >
+          <label className="block  text-sm font-bold mb-2" htmlFor="otp">
             OTP
           </label>
           <input
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
             id="otp"
             type="number"
             value={otp}
