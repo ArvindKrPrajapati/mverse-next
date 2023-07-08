ef62cc7419e9a7696dbc95070efb0d8d6a78744f
Arvind Kumar <arvind@earnnest.me>
2023-07-08 21:58:02 +0530

    tried loading component for route

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,30 +1,10 @@
-"use client";
-import userCurrentUser from "@/hooks/userCurrentUser";
-import { useRouter } from "next/navigation";
+import Logout from "@/components/Logout";
 import React from "react";
-import toast from "react-hot-toast";
 type props = {
   params: {
     id: string;
   };
 };
 export default function Profile({ params }: props) {
-  const { logout } = userCurrentUser();
-  const router = useRouter();
-
-  const logmeout = () => {
-    logout();
-    toast.success("logout success");
-    router.refresh();
-  };
-  return (
-    <div className="p-3">
-      <button
-        onClick={logmeout}
-        className="text-white bg-rose-400 px-6 py-2 rounded-md"
-      >
-        logout
-      </button>
-    </div>
-  );
+  return <Logout />;
 }
---
+++ src/components/Logout/index.tsx
@@ -0,0 +1,26 @@
+"use client";
+import userCurrentUser from "@/hooks/userCurrentUser";
+import { useRouter } from "next/navigation";
+import React from "react";
+import toast from "react-hot-toast";
+
+export default function Logout() {
+  const { logout } = userCurrentUser();
+  const router = useRouter();
+
+  const logmeout = () => {
+    logout();
+    toast.success("logout success");
+    router.refresh();
+  };
+  return (
+    <div className="p-3">
+      <button
+        onClick={logmeout}
+        className="text-white bg-rose-400 px-6 py-2 rounded-md"
+      >
+        logout
+      </button>
+    </div>
+  );
+}
