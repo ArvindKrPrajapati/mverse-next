07d1dc997d7b9f59ced9b399095a1efd1c7badcb
Arvind Kumar <arvind@earnnest.me>
2023-07-10 21:18:31 +0530

    added await while setting cookie in logout

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,12 +1,26 @@
 import { setCookiesOptions } from "@/lib/constants";
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
+  const cookieStore = cookies();
+  cookieStore.set({
+    name: "token",
+    value: " ",
+    httpOnly: true,
+    path: "/",
+  });
+  cookieStore.set({
+    name: "user",
+    value: " ",
+    httpOnly: true,
+    path: "/",
+  });
   const res = NextResponse.json({
     success: true,
     data: { message: "logged out successfully" },
   });
-  await res.cookies.set("user", "user", setCookiesOptions);
-  await res.cookies.set("token", "token", setCookiesOptions);
+  // await res.cookies.set("user", "user", setCookiesOptions);
+  // await res.cookies.set("token", "token", setCookiesOptions);
   return res;
 }
