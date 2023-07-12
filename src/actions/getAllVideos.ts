3d8b99c59e7fc1c9f98b45e7b18edbd0b377fce6
ArvindKrPrajapati <arvind@earnnest.me>
2023-07-12 21:44:52 +0530

    user model just imported only

--- src/actions/getAllVideos.ts
+++ src/actions/getAllVideos.ts
@@ -8,8 +8,8 @@ export async function getAllVideos(skip = 0, limit = constLimit) {
     // connect db
     await dbConnect();
     // justr to connect with user modal cause it was giving error to find videos because of ref
-    const res = await User.findOne();
-    console.log(res);
+    // const res = await User.findOne();
+    // console.log(res);
 
     // get user
     const data = await Video.find()
