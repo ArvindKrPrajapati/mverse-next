a9f5a9a55116377805de5ac11066dc298966de48
ArvindKrPrajapati <arvind@earnnest.me>
2023-07-12 14:42:01 +0530

    still not was fixed

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -8,7 +8,7 @@ export default function Container({
   children: React.ReactNode;
 }) {
   return (
-    <div className={`max-w-7xl px-6 sm:px-8 lg:px-10 mx-auto ${className}`}>
+    <div className={`px-6 sm:px-8 lg:px-10 mx-auto ${className}`}>
       {children}
     </div>
   );
