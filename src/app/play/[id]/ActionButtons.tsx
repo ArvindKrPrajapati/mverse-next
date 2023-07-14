ebda55560f10412039fee510f46913ee73ddee86
ArvindKrPrajapati <arvind@earnnest.me>
2023-07-14 15:29:01 +0530

    enabled shared button

--- src/app/play/[id]/ActionButtons.tsx
+++ src/app/play/[id]/ActionButtons.tsx
@@ -3,6 +3,20 @@ import { DislikeIcon, LikeIcon, ShareIcon } from "@/components/_icons";
 import React from "react";
 
 export default function ActionButtons() {
+  const handleShare = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: "Your shared title",
+          text: "Your shared content",
+          url: window.location.href,
+        });
+      } catch (error) {
+        console.error("Sharing failed:", error);
+      }
+    }
+  };
+
   return (
     <div className="flex mt-4 items-center">
       <button className="flex rounded-full bg-slate-300 dark:bg-transparent h-full px-5 py-1 text-sm">
@@ -13,7 +27,10 @@ export default function ActionButtons() {
         <DislikeIcon width={20} />
         <p className="ml-2">1K</p>
       </button>
-      <button className="flex rounded-full bg-slate-300 dark:bg-transparent h-full px-5 py-1 text-sm ml-2">
+      <button
+        onClick={handleShare}
+        className="flex rounded-full bg-slate-300 dark:bg-transparent h-full px-5 py-1 text-sm ml-2"
+      >
         <ShareIcon width={20} />
         <p className="ml-2">share</p>
       </button>
