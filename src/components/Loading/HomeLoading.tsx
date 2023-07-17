dd64eac81f88edb08197008c15cd6bcc213e471e
ArvindKrPrajapati <arvind@earnnest.me>
2023-07-17 23:38:23 +0530

    added key in homeloading

--- src/components/Loading/HomeLoading.tsx
+++ src/components/Loading/HomeLoading.tsx
@@ -5,7 +5,7 @@ export default function HomeLoading() {
     <main className="h-[calc(100vh-50px)]  overflow-hidden -mt-2 md:mt-0">
       <div className="flex flex-wrap justify-center md:mt-5">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map((key) => (
-          <div className="md:mx-1 my-2">
+          <div className="md:mx-1 my-2" key={key}>
             <div className="animate-pulse sm:rounded-lg w-[100vw] md:w-[39vw] lg:w-[35vw] xl:max-w-[270px] aspect-[16/10] bg-slate-200 dark:bg-neutral-800"></div>
             <div className={`flex m-2`}>
               <div
