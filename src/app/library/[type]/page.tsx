c0b5d702bb60ebe22413ec42385913e72ed56e5c
ArvindKrPrajapati <arvind@earnnest.me>
2023-07-23 01:20:01 +0530

    hisrtory done

---
+++ src/app/library/[type]/page.tsx
@@ -0,0 +1,28 @@
+import { getHistory } from "@/actions/getHistory";
+import CardList from "@/components/CardList";
+import Container from "@/components/Container";
+import ProtectedContainer from "@/components/Container/ProtectedContainer";
+import { getCurrentUser } from "@/lib/serverCookies";
+import React from "react";
+
+export default async function ShowList({ params }: any) {
+  const currentUser = getCurrentUser();
+  let data: any = [];
+  let url: string = "";
+  if (params.type === "history") {
+    data = await getHistory(currentUser?._id);
+    url = "/api/library/history";
+  }
+  return (
+    <ProtectedContainer byId={true}>
+      <Container>
+        <div className="p-4">
+          <p className="font-bold capitalize pb-2  border-b dark:border-gray-800 border-gray-200">
+            {params.type}
+          </p>
+        </div>
+        <CardList horizontal={true} data={data} loadMoreFromUrl={url} />
+      </Container>
+    </ProtectedContainer>
+  );
+}
--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -2,7 +2,7 @@ import { getHistory } from "@/actions/getHistory";
 import Card from "@/components/Card";
 import Container from "@/components/Container";
 import ProtectedContainer from "@/components/Container/ProtectedContainer";
-import { ChevronRight } from "@/components/_icons";
+import { ChevronRight, DislikeIcon, LikeIcon } from "@/components/_icons";
 import { getCurrentUser } from "@/lib/serverCookies";
 import Link from "next/link";
 import React from "react";
@@ -18,17 +18,36 @@ export default async function LibraryPage() {
     <ProtectedContainer byId={true}>
       <Container>
         <main className="p-3">
-          <Link href="/history" className="flex justify-between items-center">
+          <Link
+            href="/library/history"
+            className="flex justify-between items-center"
+          >
             <p className="text-base font-bold">History</p>
             <ChevronRight />
           </Link>
         </main>
         <div className="px-3 flex md:flex-wrap gap-3 md:gap-0 overflow-auto">
           {data.map((item: any, index: number) => (
-            <div className="md:p-2 sm:w-1/2 lg:w-1/3 min-[1200px]:w-1/4 min-w-[200px]">
+            <div className="md:p-2 sm:w-1/3 lg:w-1/4 min-[1200px]:w-1/5 min-w-[200px]">
               <Card key={index} item={item} history={true} />
             </div>
           ))}
+        </div>
+        <div className="mt-3 p-5 border-t dark:border-gray-800 border-gray-200">
+          <Link
+            href="/library/liked"
+            className="flex gap-4 items-center py-3 -full"
+          >
+            <LikeIcon active={true} />
+            <p className="text-base font-bold">Liked Videos</p>
+          </Link>
+          <Link
+            href="/library/disliked"
+            className="flex gap-4 items-center py-3 w-full"
+          >
+            <DislikeIcon active={true} />
+            <p className="text-base font-bold">Disliked Videos</p>
+          </Link>
         </div>
       </Container>
     </ProtectedContainer>
